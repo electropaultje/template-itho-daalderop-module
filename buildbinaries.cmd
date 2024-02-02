9bb378607c0928c977df136e9270f123e545096d
Leo Schelvis <leo.schelvis@gmail.com>
2024-02-02 08:29:00 +0100

    Updated build file for PCB2.2

--- buildbinaries.cmd
+++ buildbinaries.cmd
@@ -4,3 +4,8 @@ ESPHOME compile ithodaalderop-2relay.yaml
 COPY .esphome\build\ithodaalderop\.pioenvs\ithodaalderop\firmware.bin .\ithodaalderop-2relay.bin /Y /V
 ESPHOME compile ithodaalderop-4relay.yaml
 COPY .esphome\build\ithodaalderop\.pioenvs\ithodaalderop\firmware.bin .\ithodaalderop-4relay.bin /Y /V
+
+ESPHOME compile ithodaalderop-v2-2relay.yaml
+COPY .esphome\build\ithodaalderop\.pioenvs\ithodaalderop\firmware.bin .\ithodaalderop-v2-2relay.bin /Y /V
+ESPHOME compile ithodaalderop-v2-4relay.yaml
+COPY .esphome\build\ithodaalderop\.pioenvs\ithodaalderop\firmware.bin .\ithodaalderop-v2-4relay.bin /Y /V
